e9d8991a2acf33861dce0620f507a8f05afcc1fa
MemmedzadeAygun <memmedzadeaygun2004@gmail.com>
2025-08-25 19:25:10 +0400

    create user profile

---
+++ js/profile.js
@@ -0,0 +1,10 @@
+function loadOnUser(){
+    let logedInUser = JSON.parse(localStorage.getItem('logedInUser'));
+
+    document.getElementById('user-name').textContent = logedInUser.name;
+    document.getElementById('user-surname').textContent = logedInUser.surname;
+    document.getElementById('user-email').textContent = logedInUser.email;
+    document.getElementById('user-username').textContent = logedInUser.username;
+}
+
+loadOnUser();
